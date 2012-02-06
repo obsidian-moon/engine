67c80efa2070273bcf3461d550597aa73f79b2ad
Alfonso Martinez <corrupted.rogues@gmail.com>
2012-02-06 15:48:27 -0600

    Started a redesign with the test-driven development method.

--- ObsidianMoonCoreTest.php
+++ ObsidianMoonCoreTest.php
@@ -6,8 +6,8 @@ class ObsidianMoonTest extends PHPUnit_Framework_TestCase {
 	public function setUp(){ }  
 	public function tearDown(){ } 
 	public function StartTest() {
-		$this->core = new obsidian_moon_core();
-		$this->assertGreaterThan(0,$this->core->time);
+		$this->core = new ObsidianMoonCore();
+		$this->assertGreaterThan(0,$this->core->systime);
 		$this->assertEqual(false,$this->core->is_ajax);
 	}
 }
