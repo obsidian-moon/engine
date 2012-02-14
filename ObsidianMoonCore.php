f5037a96e86b748563791f93911f45a9e8aedad7
Alfonso Martinez <corrupted.rogues@gmail.com>
2012-02-14 17:21:43 -0600

    Added the classes and views methods back in, now that our system is better suited for a clean creation. Also I modified the system to also handle the new conf_* variables.

--- ObsidianMoonCore.php
+++ ObsidianMoonCore.php
@@ -58,11 +58,91 @@ class ObsidianMoonCore
 		}
 	}
 	
-	public function classes($key, $value=NULL) {
-		
+	function classes($class, $alternate_name="",$p3_cname=null)
+	{
+		/**
+		 * This function will load classes as needed for the user to use.
+		 * It will allow them to be accessible via $core->classname.
+		 * 
+		 * @param $class - This is the file and class name of the class being called.
+		 * @param $alternate_name - This is what the user would like to name the variable if available.
+		 */
+		if (preg_match('/\//', $class))
+		{
+			$class_name = explode('/', $class);
+			$class_name = end($class_name);
+		}
+		elseif ($p3_cname !== null)
+		{
+			$class_name = $p3_cname;
+		}
+		else
+		{
+			$class_name = $class;
+		}
+
+		if (preg_match('/^core\//', $class)) 
+			$classes_location = $this->conf_core.'classes/'.$class_name.'.php';
+		elseif (preg_match('/^third_party\//', $class)) 
+			$classes_location = $this->conf_libs.$class.'.php';
+		else 
+			$classes_location = $this->conf_libs.'classes/'.$class.'.php';		
+		$configs_location = $this->conf_libs.'configs/'.$class.'.php';
+		if (file_exists($classes_location))
+		{
+			include($classes_location);
+			if (class_exists($class_name))
+			{
+				if (file_exists($configs_location))
+				{
+					include($configs_location);
+				}
+				if ($alternate_name == "")
+				{
+					$alternate_name = $class_name;
+				}
+				if (!isset($this->$alternate_name)) {
+					if (isset($config) && $config !== null)
+					{	
+						$this->$alternate_name = new $class_name($config);				
+					}
+					else
+					{	
+						$this->$alternate_name = new $class_name();
+					}
+					$this->$alternate_name->core =& $this;
+					if (method_exists($this->$alternate_name, 'om_start')) {
+						$this->$alternate_name->om_start();
+					}	
+					return true;
+				}
+				else
+				{
+					$this->error[] = "\$this->$alternate_name has already been set, could not reinstanciate it!";
+				}
+			}
+		}	
 	}
-	
-	public function views() {
-		
+
+	function views($_view, $_data=NULL, $_return=FALSE)
+	{
+		/**
+		 * This method loads a 'view' and implants the data into it,
+		 * and if needed returns that value to be included in other views.
+		 * 
+		 * @param $_view - name of the view to be called.
+		 * @param $_data - data that can be passed into the view to populate existing variables.
+		 * @param $_return - if this is set to true it will pass the value out to user otherwise append to the output buffer.
+		 */
+		if (file_exists($this->conf_libs.'views/'.$_view.'.php'))
+		{
+			if ($_data !== NULL) extract($_data, EXTR_OVERWRITE);
+			ob_start();
+			include($this->conf_libs.'views/'.$_view.'.php');
+			$buffer = ob_get_contents();
+			@ob_end_clean();
+			if ($_return == TRUE) return $buffer;
+			else $this->output .= $buffer;
+		}
 	}
 }
