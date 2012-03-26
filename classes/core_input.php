9c03fc43bd6ce925ba7f1f429dc86668f05e6be6
Alfonso Martinez <corrupted.rogues@gmail.com>
2012-03-26 11:03:23 -0500

    Updated the Facebook Enhanced library and made some changes to core input class. Signed-off-by:Alfonso Martinez <corrupted.rogues@gmail.com>

--- classes/core_input.php
+++ classes/core_input.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 
  * Obsidian Moon Engine presented by Dark Prospect Games
@@ -7,58 +8,51 @@
  * 
  */
 include 'core_security.php';
-class core_input
-{
+
+class core_input {
+
 	/**
 	 * Based on CodeIgniters Input Class
 	 */
 	public $core;
+
 	function __construct() {
 		// We are going to use the CodeIgniter security class until I can make one for myself.
 		$this->security = new core_security();
 	}
-	
-	function _fetch_from_array(&$array, $index = '', $xss_clean = FALSE)
-	{
-		if ( ! isset($array[$index]))
-		{
+
+	function _fetch_from_array(&$array, $index = '', $xss_clean = FALSE) {
+		if (!isset($array[$index])) {
 			return FALSE;
 		}
 
-		if ($xss_clean === TRUE)
-		{
+		if ($xss_clean === TRUE) {
 			return $this->security->xss_clean($array[$index]);
 		}
 
 		return $array[$index];
 	}
-	
-	function get($index)
-	{
-		if ($index === NULL AND ! empty($_GET))
-		{
+
+	function get($index) {
+		if ($index === NULL AND !empty($_GET)) {
 			$get = array();
 
 			// loop through the full _GET array
-			foreach (array_keys($_GET) as $key)
-			{
+			foreach (array_keys($_GET) as $key) {
 				$get[$key] = $this->_fetch_from_array($_GET, $key, $xss_clean);
 			}
 			return $get;
 		}
 		return $this->_fetch_from_array($_GET, $index, $xss_clean);
 	}
-	
-	function post($index = NULL, $xss_clean = FALSE, $game = NULL)
-	{
+
+	function post($index = NULL, $xss_clean = FALSE, $game = NULL) {
 		// Check if a field has been provided
-		if ($index === NULL AND ! empty($_POST))
-		{
+		if ($index === NULL AND !empty($_POST)) {
 			$post = array();
 
 			// Loop through the full _POST array and return it
-			foreach (array_keys($_POST) as $key)
-			{
+			foreach (array_keys($_POST) as $key) {
 				$post[$key] = $this->_fetch_from_array($_POST, $key, $xss_clean);
 			}
 			return $post;
@@ -69,41 +63,33 @@ class core_input
 		}
 		return $value;
 	}
-	
-	function get_post($index = '', $xss_clean = FALSE)
-	{
-		if ( ! isset($_POST[$index]) )
-		{
+
+	function get_post($index = '', $xss_clean = FALSE) {
+		if (!isset($_POST[$index])) {
 			return $this->get($index, $xss_clean);
-		}
-		else
-		{
+		} else {
 			return $this->post($index, $xss_clean);
 		}
-	}	
-	
-	function cookie($index = '', $xss_clean = FALSE)
-	{
+	}
+
+	function cookie($index = '', $xss_clean = FALSE) {
 		return $this->_fetch_from_array($_COOKIE, $index, $xss_clean);
 	}
-	
-	function server($index = '', $xss_clean = FALSE)
-	{
+
+	function server($index = '', $xss_clean = FALSE) {
 		return $this->_fetch_from_array($_SERVER, $index, $xss_clean);
 	}
-	
-	function session($index = '', $xss_clean = FALSE)
-	{
+
+	function session($index = '', $xss_clean = FALSE) {
 		return $this->_fetch_from_array($_SESSION, $index, $xss_clean);
 	}
-	
-	function set_session($index = '', $value = '')
-	{
+
+	function set_session($index = '', $value = '') {
 		return $_SESSION[$index] = $value;
 	}
-	
-	function unset_session($index = '')
-	{
+
+	function unset_session($index = '') {
 		unset($_SESSION[$index]);
 	}
+
 }
